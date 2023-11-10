03396ebddab30ba674a57a073c38cb5752b189de
Ola <cencora.aleksandra@gmail.com>
2023-11-10 16:42:33 +0100

    Add styles, navbar, footer and posts to store

---
+++ src/components/views/Footer/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="text-center text-muted">
+            <p>Copyright &#169; BlogApp 2023</p>
+        </div>
+    );
+};
+
+export default Footer;
