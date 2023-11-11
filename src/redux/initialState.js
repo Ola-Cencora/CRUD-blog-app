d01701bf7e9ed801ab97e98723bed5c2662b3af6
Ola <cencora.aleksandra@gmail.com>
2023-11-11 13:07:52 +0100

    Add posts to home page

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -13,24 +13,24 @@ const initialState = {
             title: 'Article title 2',
             shortDescription: 'Short description of the article...',
             content: 'Main content of the article',
-            publishedDate: '02-02-2022',
-            author: 'John Doe'
+            publishedDate: '02-08-2002',
+            author: 'Amanda Doe'
         },
         {
             id: '3',
             title: 'Article title 3',
             shortDescription: 'Short description of the article...',
             content: 'Main content of the article',
-            publishedDate: '02-02-2022',
-            author: 'John Doe'
+            publishedDate: '26-02-2023',
+            author: 'Harry Potter'
         },
         {
             id: '4',
             title: 'Article title 4',
             shortDescription: 'Short description of the article...',
             content: 'Main content of the article',
-            publishedDate: '02-02-2022',
-            author: 'John Doe'
+            publishedDate: '07-12-2007',
+            author: 'Gandalf'
         }
     ]
 };
